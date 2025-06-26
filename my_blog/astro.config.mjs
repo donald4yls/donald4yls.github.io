08279b96d3053033ebc74a7cf75fa915b0632c65
Donald Yu <longsheng.yu@lego.com>
2025-06-26 16:21:44 +0800

    Update astro.config.mjs

--- my_blog/astro.config.mjs
+++ my_blog/astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://donald4yls.github.io/",
-	base: "my_blog/",
+	base: "/my_blog/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
