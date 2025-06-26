53c6f8094289d4c5c1d2525b7e2e20b35cafbb70
Donald Yu <longsheng.yu@lego.com>
2025-06-26 16:11:09 +0800

    Update folder.

--- my_blog/src/config.ts
+++ my_blog/src/config.ts
@@ -46,33 +46,28 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/donald4yls", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/donald-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Donald Yu",
+	bio: "A software engineer who loves to share knowledge and experiences.",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+			name: "LinkedIn",
+			icon: "fa6-brands:linkedin", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			url: "linkedin.com/in/longsheng-yu-bb143795",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/donald4yls",
 		},
 	],
 };
---
+++ my_blog/src/content/spec/about.md
@@ -0,0 +1,8 @@
+# About Me
+
+Experienced Software Engineer with a strong background in full-stack development. Proven track record in
+leading technical initiatives, designing scalable architectures, and delivering enterprise-grade solutions.
+Integrating AI technologies, including LLMs such as OpenAI’s GPT, into automation systems to develop
+intelligent assistants, enhancing user interaction and decision-making. Recently expanded responsibilities to
+include project coordination and team mentorship, demonstrating strong leadership potential and
+cross-functional communication skills.
